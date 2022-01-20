71dcf7ee13cbfec6f81eccb1b596ee6cde25ea27
Tony Findeisen <tony.findeisen@onfido.com>
2022-01-20 19:52:27 +0100

    resolve properties

--- src/main/java/com/onfido/qa/configuration/Property.java
+++ src/main/java/com/onfido/qa/configuration/Property.java
@@ -1,14 +1,40 @@
 package com.onfido.qa.configuration;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.PARAMETER})
-public @interface Property {
-    String value();
+import java.util.Properties;
 
-    boolean resolveVars() default true;
+@SuppressWarnings("StaticCollection")
+public final class Property {
+
+    private static final Properties properties;
+    private static final TemplateProperties templateProperties;
+
+    static {
+        properties = new FallbackPropertyReader().resolveProperties();
+        templateProperties = new TemplateProperties(properties);
+    }
+
+    private Property() {
+    }
+
+    public static Properties properties() {
+        return properties;
+    }
+
+    public static String get(String key) {
+        return get(key, null);
+    }
+
+    public static String get(String key, String defaultValue) {
+        return get(key, defaultValue, true);
+    }
+
+    public static String raw(String key, String defaultValue) {
+        return get(key, defaultValue, false);
+    }
+
+    private static String get(String key, String defaultValue, boolean resolve) {
+        return (resolve ? templateProperties : properties).getProperty(key, defaultValue);
+    }
+
 }
