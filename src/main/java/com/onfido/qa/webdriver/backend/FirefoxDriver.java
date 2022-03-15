c80940e71b95a55d422ddaa353a939d5aa4bf58b
Tony Findeisen <tony.findeisen@onfido.com>
2022-03-15 11:54:39 +0100

    added support to specify the geckodriver via browserPath

--- src/main/java/com/onfido/qa/webdriver/backend/FirefoxDriver.java
+++ src/main/java/com/onfido/qa/webdriver/backend/FirefoxDriver.java
@@ -3,14 +3,24 @@ package com.onfido.qa.webdriver.backend;
 import org.openqa.selenium.firefox.GeckoDriverService;
 import org.openqa.selenium.remote.service.DriverService;
 
+import java.io.File;
 import java.util.Properties;
+
+import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 public class FirefoxDriver implements DriverServiceFactory {
     @Override
     public DriverService createDriverService(Properties properties, boolean forceLog) {
 
-        return new GeckoDriverService.Builder()
-                .usingAnyFreePort()
-                .build();
+        var builder = new GeckoDriverService.Builder()
+                .usingAnyFreePort();
+
+        var browserPath = properties.getProperty("geckoDriverPath");
+
+        if (!isEmpty(browserPath)) {
+            builder.usingDriverExecutable(new File(browserPath));
+        }
+
+        return builder.build();
     }
 }
